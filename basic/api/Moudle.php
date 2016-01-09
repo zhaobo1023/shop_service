8128336a556376d9172fbb032bc0e5f944060938
zhaobo1023 <zhaobo1023@gmail.com>
2016-01-09 22:50:06 +0800

    account

--- basic/api/Moudle.php
+++ basic/api/Moudle.php
@@ -1,3 +1,14 @@
-<?php
+mespace app\modules\forum;
+
+class Module extends \yii\base\Module
+{
+    public function init()
+    {
+        parent::init();
+
+        $this->params['foo'] = 'bar';
+        // ...  其他初始化代码 ...
+     }
+}
 
 
--- basic/api/controllers/AccountController.php
+++ basic/api/controllers/AccountController.php
@@ -1,3 +1,15 @@
 <?php
 
+namespace app\modules\forum\controllers;
 
+use yii\web\Controller;
+
+class AccountController extends Controller
+{
+    // ...
+    public function index()
+    {
+        echo 123;
+
+    }
+}
