19bfe3fa096ca0c6650993cb0eb8005ea268b3e9
zhaobo1023 <zhaobo1023@gmail.com>
2016-01-10 14:05:44 +0800

    ap

--- basic/api/index.php
+++ basic/api/index.php
@@ -1,15 +1,15 @@
 <?php
 
 // comment out the following two lines when deployed to production
-defined('yii_debug') or define('yii_debug', true);
-defined('yii_env') or define('yii_env', 'dev');
+//defined('yii_debug') or define('yii_debug', true);
+//defined('yii_env') or define('yii_env', 'dev');
 //
-require(__dir__ . '/../vendor/autoload.php');
-require(__dir__ . '/../vendor/yiisoft/yii2/yii.php');
+//require(__dir__ . '/../vendor/autoload.php');
+//require(__dir__ . '/../vendor/yiisoft/yii2/yii.php');
 //
-$config = require(__dir__ . '/../config/api.php');
+//$config = require(__dir__ . '/../config/api.php');
 //
-(new yii\api\application($config))->run();
+//(new yii\api\application($config))->run();
 
 
-/* echo 'hello'; */
+echo 'hello'; 
